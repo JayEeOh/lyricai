8e23be1d646073302fd2de577fc6c655413f2627
Verkeano <Alanhild715@gmail.com>
2024-07-13 19:13:36 +0100

    improving input

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import './App.css';
 function App() {
   return (
     <main>
-    <head>
-      <title>LyricAI</title>
-    </head>
     <body>
       <div className="App">
         <header class="App-header">
+          <img src="./logo.svg" alt="Logo" />
           <h1>LyricAI</h1>
           <h2>Search for lyrics</h2>
         </header>	
         <header class="form">
-          <input type="text" placeholder="Search for lyrics" />
+          <textarea name="lyrics" class="lyrics"></textarea>
         </header>
       </div>
     </body>
