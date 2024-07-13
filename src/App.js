f9a79a90a5a06c3f7ede6ed10a48bc33ab2d2054
Verkeano <Alanhild715@gmail.com>
2024-07-13 15:29:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header class="form">
-        <input type="text" placeholder="Search for lyrics" />
-      </header>
-    </div>
+    <main>
+    <head>
+      <title>LyricAI</title>
+    </head>
+    <body>
+      <div className="App">
+        <header class="App-header">
+          <h1>LyricAI</h1>
+          <h2>Search for lyrics</h2>
+        </header>	
+        <header class="form">
+          <input type="text" placeholder="Search for lyrics" />
+        </header>
+      </div>
+    </body>
+    </main>
   );
 }
 
