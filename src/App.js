8ee78c87f6a51c0f8ef0f1ab8d8afeab493619a2
Verkeano <Alanhild715@gmail.com>
2024-07-13 18:57:15 +0100

    manual revert

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+    <head>
+      <title>LyricAI</title>
+    </head>
+    <body>
+      <div className="App">
+        <header class="App-header">
+          <h1>LyricAI</h1>
+          <h2>Search for lyrics</h2>
+        </header>	
+        <header class="form">
+          <input type="text" placeholder="Search for lyrics" />
+        </header>
+      </div>
+    </body>
+    </main>
+  );
+}
+
+export default App;
